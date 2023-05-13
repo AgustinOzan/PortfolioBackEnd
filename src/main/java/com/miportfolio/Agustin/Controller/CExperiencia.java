5fb98a830c3b7d921ab5a7882cd0d5929f5360cb
Agustin Ozan <agustinozan456@gmail.com>
2023-05-13 14:28:14 -0300

    corrigiendo los CrossOrigin

--- src/main/java/com/miportfolio/Agustin/Controller/CExperiencia.java
+++ src/main/java/com/miportfolio/Agustin/Controller/CExperiencia.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("explab")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins={"http://localhost:4200","https://agusozanfrontend.web.app"})
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
